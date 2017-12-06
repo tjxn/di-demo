78705426a6302401ddf1dafb20f74bb74f210383
Trevor Jackson <trevorlogin@gmail.com>
2017-12-05 19:56:42 -0800

    Section 3 Lecture 29 - Spring Context demo

--- src/main/java/ca/trevorjackson/didemo/DiDemoApplication.java
+++ src/main/java/ca/trevorjackson/didemo/DiDemoApplication.java
@@ -1,12 +1,16 @@
 package ca.trevorjackson.didemo;
 
+import ca.trevorjackson.didemo.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(DiDemoApplication.class, args);
+		MyController controller = (MyController) applicationContext.getBean("myController");
+		controller.hello();
 	}
 }
---
+++ src/main/java/ca/trevorjackson/didemo/controllers/MyController.java
@@ -0,0 +1,15 @@
+package ca.trevorjackson.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Trevor Jackson
+ * December 05, 2017
+ **/
+@Controller
+public class MyController {
+
+    public void hello(){
+        System.out.println("Hello World");
+    }
+}
